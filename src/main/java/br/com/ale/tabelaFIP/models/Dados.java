448c9326ae270137372b7c16d0f8d63ac4a75d2a
Alexandre Teixeira <alextp@gmail.com>
2025-03-30 23:44:01 -0300

    Criação da po e projeto completo

---
+++ src/main/java/br/com/ale/tabelaFIP/models/Dados.java
@@ -0,0 +1,4 @@
+package br.com.ale.tabelaFIP.models;
+
+public record Dados(String codigo, String nome) {
+}
---
+++ src/main/java/br/com/ale/tabelaFIP/services/IConverteDados.java
@@ -0,0 +1,10 @@
+package br.com.ale.tabelaFIP.services;
+
+import java.util.List;
+
+public interface IConverteDados {
+    <T> T obterDados(String json, Class<T> classe);
+
+    <T> List<T> obterLista(String json, Class<T> classe);
+    //
+}
